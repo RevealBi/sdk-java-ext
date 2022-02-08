e1c3bea6aa045302cf1c328da1d2f226171ccf1a
Mauro <mlopez@infragistics.com>
2022-02-08 17:49:04 -0300

    OAuthResource was returning application/json as the content-type for an endpoint that should be text/plain

--- reveal-ext-rest/src/main/java/io/revealbi/sdk/ext/rest/OAuthResource.java
+++ reveal-ext-rest/src/main/java/io/revealbi/sdk/ext/rest/OAuthResource.java
@@ -16,6 +16,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
@@ -53,6 +54,7 @@ public class OAuthResource extends BaseResource {
 	
 	@Path("/{providerType}/auth/{dataSourceId}")
 	@GET
+	@Produces("text/plain")
 	public Response doAuth(@PathParam("providerType") OAuthProviderType providerType, @PathParam("dataSourceId") String dataSourceId, @QueryParam("finalUrl") String finalUrl) throws URISyntaxException {
 		if (dataSourceId != null && dataSourceId.equals("_new")) {
 			dataSourceId = null;
