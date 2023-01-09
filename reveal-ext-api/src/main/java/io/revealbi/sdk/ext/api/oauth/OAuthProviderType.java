7f53dc75f072739ad0be291fb60559095b95c801
MLopez <mlopez@infragistics.com>
2023-01-09 11:39:26 -0300

    Preserve ordinal of enum values in case any sdk user is relying on those when persisting

--- reveal-ext-api/src/main/java/io/revealbi/sdk/ext/api/oauth/OAuthProviderType.java
+++ reveal-ext-api/src/main/java/io/revealbi/sdk/ext/api/oauth/OAuthProviderType.java
@@ -4,13 +4,13 @@ import com.infragistics.reportplus.datalayer.api.ProviderKeys;
 
 public enum OAuthProviderType {
 	GOOGLE_ANALYTICS(ProviderKeys.googleAnalyticsProviderKey),
-	GOOGLE_ANALYTICS_4(ProviderKeys.googleAnalytics4ProviderKey),
 	GOOGLE_BIG_QUERY(ProviderKeys.bigQueryProviderKey),
 	GOOGLE_DRIVE(ProviderKeys.googleDriveProviderKey),
 	ONE_DRIVE(ProviderKeys.oneDriveProviderKey),
 	DROPBOX(ProviderKeys.dropboxProviderKey),
 	BOX(ProviderKeys.boxProviderKey),
-	GOOGLE_SEARCH_CONSOLE(ProviderKeys.googleSearchProviderKey);
+	GOOGLE_SEARCH_CONSOLE(ProviderKeys.googleSearchProviderKey),
+	GOOGLE_ANALYTICS_4(ProviderKeys.googleAnalytics4ProviderKey);
 	
 	private String providerId;
 	private OAuthProviderType(String providerId) {
